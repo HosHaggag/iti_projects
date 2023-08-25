872bd2a03b7d679d5088632c00192accc83c4472
haggagdev <haggagdev@gmail.com>
2023-08-25 21:33:41 +0300

    fix english syntax issues

--- lab_2.py
+++ lab_2.py
@@ -3,40 +3,40 @@
 # generate an array of a specific length filled with integer numbers 
 # increased by one from start.
 
-# def generate_array(length, start):
-#     array = []
-#     for i in range(length):
-#         array.append(start)
-#         start += 1
-#     return array
+def generate_array(length, start):
+    array = []
+    for i in range(length):
+        array.append(start)
+        start += 1
+    return array
 
-# print(generate_array(5, 2))
+print(generate_array(5, 2))
 
 # ● write a function that takes a number as an argument and if the 
 # number divisible by 3 return "Fizz" and if it is divisible by 5 return 
 # "buzz" and if is is divisible by both return "FizzBuzz"
 
-# def fizz_buzz(number):
-#     if number % 3 == 0 and number % 5 == 0:
-#         return "FizzBuzz"
-#     elif number % 5 == 0:
-#         return "buzz"
-#     elif number % 3 == 0:
-#         return "Fizz"
-#     else:
-#         return number
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 5 == 0:
+        return "buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    else:
+        return number
 
-# print(fizz_buzz(15))
-# print(fizz_buzz(5))
-# print(fizz_buzz(3))
+print(fizz_buzz(15))
+print(fizz_buzz(5))
+print(fizz_buzz(3))
 
 # ● Write a function which has an input of a string from user then it 
 # will return the same string reversed.
 
-# def reverse_string(string):
-#     return string[::-1]
+def reverse_string(string):
+    return string[::-1]
 
-# print(reverse_string("Hossam"))
+print(reverse_string("Hossam"))
 
 
 # ● Ask the user for his name then confirm that he has entered his 
@@ -44,16 +44,16 @@
 # his email and print all this data (Bonus) check if it is a valid email 
 # or not
 
-# def get_user_data():
-#     name = input("Please enter your name: ")
-#     while name == "":
-#         name = input("Please enter valid name: ")
-#     email = input("Please enter your email: ")
-#     while email == "" or "@" not in email:
-#         email = input("Please enter valid email: ")
-#     return name, email
+def get_user_data():
+    name = input("Please enter your name: ")
+    while name == "":
+        name = input("Please enter valid name: ")
+    email = input("Please enter your email: ")
+    while email == "" or "@" not in email:
+        email = input("Please enter valid email: ")
+    return name, email
 
-# get_user_data()
+get_user_data()
 
 # ● Write a function that takes a string and prints the 
 # longest alphabetical ordered substring occurred For example, if 
@@ -84,7 +84,7 @@ def longest_substring(string):
     print(substrings)
 
 
-# longest_substring("abdulrahman")
+longest_substring("abdulrahman")
 
 
 
@@ -99,25 +99,25 @@ def longest_substring(string):
  # ○ If the user enters anything other than a number, detect their 
  # mistake, print an error message and skip to the next number.
 
-# def calculate_enter():
-#     total = 0
-#     count = 0
-#     while True:
-#         new_enter = input("enter new number : ")
-#         if new_enter == "done":
-#             print("average: ",(total/count))
-#             print("count: ",count)
-#             print("total: ",total)
-#             break
-#         elif (isinstance(new_enter ,(int,float) ) or new_enter.isdigit) and not new_enter == '':
-#             new_enter = int(new_enter)
-#             count +=1
-#             total += new_enter
-#         else :
-#             print("wrong enter please enter digit or 'done' ")
-#             print(type(new_enter))
+def calculate_enter():
+    total = 0
+    count = 0
+    while True:
+        new_enter = input("enter new number : ")
+        if new_enter == "done":
+            print("average: ",(total/count))
+            print("count: ",count)
+            print("total: ",total)
+            break
+        elif (isinstance(new_enter ,(int,float) ) or new_enter.isdigit) and not new_enter == '':
+            new_enter = int(new_enter)
+            count +=1
+            total += new_enter
+        else :
+            print("wrong enter please enter digit or 'done' ")
+            print(type(new_enter))
 
-# calculate_enter()
+calculate_enter()
 
 
 
@@ -126,7 +126,6 @@ def longest_substring(string):
  # ○ A list of words will be hardcoded in your program, out of 
  # which the interpreter will
 
-words = ("ahmed","mohammed","Noha")
 
 
 #  # ○ choose 1 random word.
@@ -139,6 +138,9 @@ words = ("ahmed","mohammed","Noha")
 #  # ○ Else the program will ask you to guess another alphabet.
 
  # ○ Give 7 turns maximum to guess the complete word.
+
+ 
+words = ("ahmed","mohammed","Noha")
 
 def hangman():
     word = words[0]
